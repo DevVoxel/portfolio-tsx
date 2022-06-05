35b2aae98efb2b01359f2ba89431dd10ddcf2da1
Voxel20 <aidensmith20@gmail.com>
2022-06-05 13:22:26 -0400

    Config Edits.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,55 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+  
   reactStrictMode: true,
+
+  api: {
+    bodyParser: false,
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/Voxel20',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/DevVoxel',
+        permanent: true,
+      },
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/users/153258046171119616',
+        permanent: true,
+      },
+      {
+        source: '/twitch',
+        destination: 'https://twitch.tv/Mealtimez',
+        permanent: true,
+      },
+      {
+        source: '/mail',
+        destination: 'mailto:aidensmith20@gmail.com',
+        permanent: true,
+      },
+      {
+        source: '/donate',
+        destination: 'https://ko-fi.com/voxel20',
+        permanent: true,
+      }
+    ]
+  },
+
+  images: {
+    domains: ["i.imgur.com", "images.ctfassets.net"],
+  },
 }
 
 module.exports = nextConfig
