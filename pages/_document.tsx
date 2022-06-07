896f5f99c6d1dd9f906eb3962820822fa5768989
Voxel20 <aidensmith20@gmail.com>
2022-06-07 18:27:37 -0400

    Font Styles Added.  ColorModeScript Theme Added.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,26 @@
-import { ColorModeScript } from '@chakra-ui/react'
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "../styles/theme";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-
-export default class Document extends NextDocument {
-  render(): React.ReactElement {
+export default class MyDocument extends NextDocument {
+  render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap"
+          rel="stylesheet"
+        />
         <Head />
         <body>
-          <ColorModeScript />
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
