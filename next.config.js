9a84268d148345391bd046f7987e7acf070b7dd6
Voxel20 <aidensmith20@gmail.com>
2022-06-16 17:28:00 -0400

    Next Config Update.

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     config.resolve.fallback = { fs: false };
     return config;
   },
-  
+
   reactStrictMode: true,
 
   api: {
@@ -15,51 +15,51 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/github',
-        destination: 'https://github.com/Voxel20',
+        source: "/github",
+        destination: "https://github.com/Voxel20",
         permanent: true,
       },
       {
-        source: '/twitter',
-        destination: 'https://twitter.com/DevVoxel',
+        source: "/twitter",
+        destination: "https://twitter.com/DevVoxel",
         permanent: true,
       },
       {
-        source: '/discord',
-        destination: 'https://discordapp.com/users/153258046171119616/',
+        source: "/discord",
+        destination: "https://discordapp.com/users/153258046171119616/",
         permanent: true,
       },
       {
-        source: '/twitch',
-        destination: 'https://twitch.tv/Mealtimez',
+        source: "/twitch",
+        destination: "https://twitch.tv/Mealtimez",
         permanent: true,
       },
       {
-        source: '/mail',
-        destination: 'mailto:aidensmith20@gmail.com',
+        source: "/mail",
+        destination: "mailto:aidensmith20@gmail.com",
         permanent: true,
       },
       {
-        source: '/donate',
-        destination: 'https://ko-fi.com/voxel20',
+        source: "/donate",
+        destination: "https://ko-fi.com/voxel20",
         permanent: true,
       },
       {
-        source: '/linkedin',
-        destination: 'https://www.linkedin.com/in/aidensmithdev/',
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/aidensmithdev/",
         permanent: true,
       },
       {
-        source: '/stackoverflow',
-        destination: 'https://stackoverflow.com/users/13132838/aiden-s',
+        source: "/stackoverflow",
+        destination: "https://stackoverflow.com/users/13132838/aiden-s",
         permanent: true,
       },
-    ]
+    ];
   },
 
   images: {
     domains: ["i.imgur.com", "images.ctfassets.net"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
