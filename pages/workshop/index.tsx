db99f6a723b8b06555022fc36a0ac924bdacd80a
Voxel20 <aidensmith20@gmail.com>
2022-06-25 22:52:45 -0400

    Workshop Page Content Updated.

--- pages/workshop/index.tsx
+++ pages/workshop/index.tsx
@@ -3,7 +3,7 @@ import Scroll from '../../components/Scroll';
 import useMediaQuery from '../../hook/useMediaQuery';
 import useColorMode from '@chakra-ui/react';
 import Head from 'next/head';
-import { Stack } from '@chakra-ui/react';
+import { Stack, Text } from '@chakra-ui/react';
 export default function Workshop() {
   const isLargerThan480 = useMediaQuery(480);
   const isLargerThan800 = useMediaQuery(800);
@@ -19,6 +19,12 @@ export default function Workshop() {
           my={['10vh', '10vh', '15vh', '15vh']}
         >
           <h1>Workshop</h1>
+          <Text>
+            This is a workshop page. I will be posting all common builds,
+            components, small projects, and more here. A search function will be
+            added accordingly eventually. This inlcudes sorting by tag of each
+            iteration.
+          </Text>
         </Stack>
       </Container>
       <Scroll />
